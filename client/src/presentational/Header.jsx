cc588901076b1e8bd1833f6756360d338a7a4fac
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2024-12-19 17:13:06 +0100

    Fix quote on mobile

--- client/src/presentational/Header.jsx
+++ client/src/presentational/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header className="flex w-5/6 mx-auto mt-4 text-center">
       <div className="w-full">
         <a href="/">
-          <h1 className="mb-8 text-4xl font-semibold md:text-6xl font-lcd">
+          <h1 className="mb-0 text-4xl font-semibold md:text-6xl font-lcd">
             shelfable
           </h1>
         </a>
