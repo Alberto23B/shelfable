fc28ae8d46ecc2dbf7c8c2fd7c79263bfbadda54
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-01-10 15:55:37 +0100

    Add a get route to app to test deployment issue

--- server/api/index.js
+++ server/api/index.js
@@ -7,6 +7,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 app.use("/api", api);
 
 //Error creation middleware
