980dc06797b1be7033c3dcd25209a365af77ffb4
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-01-13 12:13:59 +0100

    Restore locally working version

--- server/api/index.js
+++ server/api/index.js
@@ -30,8 +30,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-// app.listen(PORT, () => {
-//   console.log(`Server is listening on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 
 export default app;
