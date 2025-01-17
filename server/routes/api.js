0986126fdcfdd11b15172b5bad0e079af6ce12b5
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-01-17 15:45:06 +0100

    Change sameSite: lax to none

--- server/routes/api.js
+++ server/routes/api.js
@@ -36,7 +36,7 @@ connection(async (client) => {
       cookie: {
         secure: process.env.NODE_ENV === "production",
         httpOnly: true,
-        sameSite: "lax",
+        sameSite: "none",
         maxAge: 3600000 * 24 * 14,
       },
     })
