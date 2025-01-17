a91a4c90547833e5c67c378c9793a044f22721dd
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-01-17 09:43:22 +0100

    Change res.send to res.render @"/"

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(
 );
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Server landing page"));
+app.get("/", (req, res) => res.render("Server landing page"));
 
 app.use("/api", api);
 
