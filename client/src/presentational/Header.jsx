686a8a576a3e150dbd71f9adef93bc115674a58c
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-01-17 23:23:17 +0100

    Change font setting  to improve performance

--- client/src/presentational/Header.jsx
+++ client/src/presentational/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header className="w-1/3 mr-8 md:w-fit md:mx-4 md:mr-0 shrink">
       <div className="w-full">
         <a href="/">
-          <h1 className="mb-0 text-4xl font-semibold md:text-6xl font-lcd">
+          <h1 className="mb-0 font-sans text-4xl font-semibold md:text-6xl">
             shelfable
           </h1>
         </a>
