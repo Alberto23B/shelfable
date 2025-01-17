6a9a18ca25de7c0956e4ee45408675da8c635d6a
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-01-18 00:06:01 +0100

    Change toggle list aspect

--- client/src/components/SwitchList.jsx
+++ client/src/components/SwitchList.jsx
@@ -15,22 +15,25 @@ export default function SwitchList() {
   };
 
   return (
-    <div className="relative top-[3px] w-full text-end sm:right-2 ml-4 rounded-lg right-0">
+    <div className="relative top-[3px] w-full text-end sm:right-4 ml-4 rounded-lg right-0">
       <div className="relative inline-block w-10 mr-2 align-middle transition duration-200 ease-in select-none">
         <input
           type="checkbox"
           name="toggle"
           id="toggle"
-          className="absolute block w-6 h-6 bg-white border-4 rounded-full appearance-none cursor-pointer toggle-checkbox"
+          className="absolute block w-8 h-8 bg-white border-4 rounded-full appearance-none cursor-pointer toggle-checkbox"
           onChange={handleClick}
           checked={display === "list"}
         />
         <label
           htmlFor="toggle"
-          className="block h-6 overflow-hidden bg-gray-300 rounded-full cursor-pointer toggle-label"
+          className="block w-12 h-8 overflow-hidden bg-gray-300 rounded-full cursor-pointer toggle-label"
         ></label>
       </div>
-      <label htmlFor="toggle" className="text-xs text-black dark:text-white">
+      <label
+        htmlFor="toggle"
+        className="p-2 pl-4 text-sm text-black dark:text-white bg-verdigris inv-border"
+      >
         List View
       </label>
     </div>
