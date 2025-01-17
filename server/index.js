22406cf1cba5c37b7586cff80ccf654bd58f05ef
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-01-17 10:04:13 +0100

    Change procfile and remove a logger

--- server/index.js
+++ server/index.js
@@ -20,7 +20,6 @@ app.use("/api", api);
 
 if (process.env.NODE_ENV === "production") {
   console.log("PRODUCTION MODE ACTIVE");
-  //app.use('/', express.static("/client/build"));
   app.use(express.static("./client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
