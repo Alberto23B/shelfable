f7a9759b6304463c6eb346488a7e3eb2123b4c2d
Alberto Biolchi <169494833+Alberto23B@users.noreply.github.com>
2025-01-31 10:31:02 +0100

    Make full html background verdigris (light)

--- client/src/presentational/Loading.jsx
+++ client/src/presentational/Loading.jsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex justify-center gap-3 mt-10 min-h-56 bg-verdigris">
+    <div className="flex justify-center gap-3 mt-10 min-h-56">
       <div className="w-2 h-2 rounded-full bg-neutral-800 dot"></div>
       <div className="w-2 h-2 rounded-full bg-neutral-800 dot"></div>
       <div className="w-2 h-2 rounded-full bg-neutral-800 dot"></div>
